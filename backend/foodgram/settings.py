4ba6504a1432595f88d5f48b7a11e75d98543302
Nikita <riki.kizh@yandex.ru>
2022-06-17 19:20:09 +0300

    filters_test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -97,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication', 
+        'rest_framework.authentication.TokenAuthentication',
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
